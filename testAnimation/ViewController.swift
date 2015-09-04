9e068f22b9e48e640f425f34e12716154c341b60
Nadohs <djsente@gmail.com>
2015-09-04 00:49:10 -0700

    fixed animation timing bug

--- testAnimation/ViewController.swift
+++ testAnimation/ViewController.swift
@@ -12,13 +12,13 @@ import UIKit
 
 func animateTime(){
     AniRun(
-          ANI(1.5){
+          ANI(1.0){
                 print("1\n")
             }
-      /> ANI(2.5){
+      /> ANI(2.0){
                 print("2\n")
             }
-      /> ANI(3.5){
+      /> ANI(3.0){
                 print("3\n")
             }
     )
@@ -41,12 +41,42 @@ func longFormAnimation(){
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var button1: UIButton!
+    
+    @IBOutlet weak var button2: UIButton!
+    
+    @IBOutlet weak var button3: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        animateTime()
-        // Do any additional setup after loading the view, typically from a nib.
+
     }
 
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+//        animateTime()
+        
+        AniRun(
+            ANI(3.0){
+                    self.button1.frame.origin.x += 100
+                }
+            /> ANI(0){
+                    self.button2.frame.origin.x += 100
+                }
+            /> ANI(5.5){
+                    self.button3.frame.origin.x += 100
+            }
+            /> ANI(3.0){
+                self.button1.frame.origin.x -= 100
+            }
+            /> ANI(5.5){
+                self.button2.frame.origin.x -= 100
+            }
+            /> ANI(5.5){
+                self.button3.frame.origin.x -= 100
+        }
+        )
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
